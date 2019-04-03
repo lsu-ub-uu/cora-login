b68240003916db581b3b7d0ba1c17d8294ade272
olovm <olov.mckie@ub.uu.se>
2019-04-03 10:27:50 +0000

    updating for use of java modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+
+module se.uu.ub.cora.apptokenverifier {
+	requires se.uu.ub.cora.gatekeepertokenprovider;
+	requires se.uu.ub.cora.json;
+	requires se.uu.ub.cora.apptokenstorage;
+	requires java.activation;
+	requires se.uu.ub.cora.httphandler;
+	requires java.ws.rs;
+	requires javax.servlet.api;
+}
