fbe08a154376b385566857dbbf68014d3b2f346e
olovm <olov.mckie@ub.uu.se>
2019-10-30 11:16:47 +0000

    updates to try to get jersey working with java 12

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,8 @@ module se.uu.ub.cora.apptokenverifier {
 	requires se.uu.ub.cora.gatekeepertokenprovider;
 	requires se.uu.ub.cora.json;
 	requires se.uu.ub.cora.apptokenstorage;
-	requires java.activation;
+	// requires java.activation;
+	requires jakarta.activation;
 	requires se.uu.ub.cora.httphandler;
 	requires java.ws.rs;
 	requires javax.servlet.api;
