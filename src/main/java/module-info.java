e37f7c4992d740d93d1123368734cda5a6232c93
olovm <olov.mckie@ub.uu.se>
2019-10-30 12:36:31 +0000

    updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module se.uu.ub.cora.apptokenverifier {
 	requires se.uu.ub.cora.gatekeepertokenprovider;
 	requires se.uu.ub.cora.json;
 	requires se.uu.ub.cora.apptokenstorage;
-	// requires java.activation;
 	requires jakarta.activation;
 	requires se.uu.ub.cora.httphandler;
 	requires java.ws.rs;
