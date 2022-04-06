538633a6a65ecc3bbbe6843278f8c40e5b824ed7
pere <pere@98ac9fd86802>
2022-04-06 14:45:46 +0000

    Adding dependency to Argon2

---
+++ src/test/java/se/uu/ub/cora/apptokenverifier/keydeviationfunction/KeyDeviationFunctionAdapterTest.java
@@ -0,0 +1,7 @@
+package se.uu.ub.cora.apptokenverifier.keydeviationfunction;
+
+public class KeyDeviationFunctionAdapterTest {
+	// https://en.wikipedia.org/wiki/Argon2
+	// https://en.wikipedia.org/wiki/Key_derivation_function
+
+}
